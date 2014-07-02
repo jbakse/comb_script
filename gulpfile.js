17eed3b78453b09e8e3430ed7ffad17383249065
Justin Bakse <jbakse@brandnewschool.com>
2014-07-02 19:22:00 -0400

    documentation upgrade

--- gulpfile.js
+++ gulpfile.js
@@ -95,8 +95,8 @@ gulp.task('language', function() {
 //Watch Files For Changes
 gulp.task('watch', function() {
 
-	gulp.watch('./src/javascript/**/*.js', ['javascript']);
-	// gulp.watch('./src/javascript/**/*.js', ['javascript-doc']);
+	// gulp.watch('./src/javascript/**/*.js', ['javascript']);
+	gulp.watch('./src/javascript/**/*.js', ['javascript-doc']);
 	gulp.watch('./src/language/*.yaml', ['language']);
 	gulp.watch('./src/style/*.less', ['style', 'styledocs']);
 	gulp.watch('./src/*.html', ['html']);
--- src/javascript/doc.js
+++ src/javascript/doc.js
@@ -4,22 +4,38 @@ var Mustache = require('mustache');
 console.log(Mustache);
 $(function() {
 
+	var regionTypes = _(language).values();
+
+	_(regionTypes).each(function(type){
+		type.required_properties = _(type.properties).filter( function(prop) {
+			return prop.required && prop.required === true;
+		});
+		
+		type.properties = _(type.properties).difference(type.required_properties);
+		
+		if (type.extends && language[type.extends]) {
+			type.inherited_properties = language[type.extends].properties;
+		}
+	});
+
+	console.log(regionTypes);
+
 	var data = {
-		regionTypes: _(language).values()
+		regionTypes: regionTypes
 	};
 
 
-	var template = $('#regionTypes').html();
-	Mustache.parse(template); // optional, speeds up future uses
-	var rendered = Mustache.render(template, data);
-	$('#regionTypes').html(rendered);
+	var template = $('#template-regionTypes').html().replace(/&gt;/g, ">");
+	var propertyTemplate = $('#template-property').html();
+	var rendered = Mustache.to_html(template, data, {prop: propertyTemplate});
+	$('#template-regionTypes').html(rendered);
 
-	var template = $('#menu').html();
-	Mustache.parse(template); // optional, speeds up future uses
-	var rendered = Mustache.render(template, data);
-	$('#menu').html(rendered);
+	var menuTemplate = $('#template-menu').html();
+	Mustache.parse(menuTemplate); // optional, speeds up future uses
+	var menuRendered = Mustache.render(menuTemplate, data);
+	$('#template-menu').html(menuRendered);
 
-	console.log("pp");
+
 	$('pre').addClass('prettyprint');
 	prettyPrint();
 
