cb140a6f4643c4e05e3dfb9473a6cd9f0124e257
Justin Bakse <jbakse@brandnewschool.com>
2014-07-18 18:26:03 -0400

    updated boolean examples

--- src/javascript/Settings.js
+++ src/javascript/Settings.js
@@ -2,7 +2,7 @@
 var _ = require('underscore');
 
 module.exports.fileURL = "./examples/box.yaml";
-module.exports.fileURL = "../yaml/tricky_rings.yaml";
+// module.exports.fileURL = "../yaml/tricky_grid.yaml";
 module.exports.previewCanvasWidth = 1200;
 module.exports.previewCanvasHeight = 1200;
 module.exports.inspectOnHover = false;
