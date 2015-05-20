63b493b754104f0d8008a795c5c58043e8f80d82
Greg Schomburg <gschomburg@gmail.com>
2015-05-20 13:18:07 -0400

    updated polygon region

--- src/javascript/region/Polygon.js
+++ src/javascript/region/Polygon.js
@@ -18,20 +18,38 @@ Polygon.prototype = Object.create(Region.prototype);
 Polygon.prototype.constructor = Polygon;
 
 Polygon.prototype.drawBuild = function(_bounds) {
-	console.log(_bounds);
-
 	// var boundsPath = new paper.Path.RegularPolygon(new paper.Point(0, 0), 8, 50);
 	// return boundsPath;
+	var polygon;
+	if (this.properties.radius){
+		console.log("props width " + this.properties.width);
+		var r = null;
+		r = this.properties.radius.toNumber("px");
+		polygon = new paper.Path.RegularPolygon(new paper.Point(_bounds.x + _bounds.width/2, _bounds.y + _bounds.height/2), this.properties.sides, r);
 
-	return this.drawBounds(_bounds);
+	}else{
+		polygon = new paper.Path.RegularPolygon(new paper.Point(_bounds.x + _bounds.width/2, _bounds.y + _bounds.height/2), this.properties.sides, _bounds.height/2);
 
-	// var boundsPath = new paper.Path.RegularPolygon(_bounds, 3, this.properties.radius || 0);
-	// return boundsPath;
+		if (this.properties.scale === "fit") {
+			polygon.scale(_bounds.size.divide(polygon.bounds.size));
+		}
+
+		if (this.properties.scale === "cover") {
+			var ratio = _bounds.size.divide(polygon.bounds.size);
+			polygon.scale(Math.max(ratio.width, ratio.height));
+		}
+
+		if (this.properties.scale === "contain") {
+			var ratio = _bounds.size.divide(polygon.bounds.size);
+			polygon.scale(Math.min(ratio.width, ratio.height));
+		}
+	}
+
+	return polygon
 };
 
 Polygon.prototype.drawBounds = function(_bounds) {
 	// return new paper.Path.RegularPolygon(new paper.Point(_bounds.x + _bounds.width/2, _bounds.y + _bounds.height/2), 8, (_bounds.width/2 | _bounds.height/2));
-	return new paper.Path.RegularPolygon(new paper.Point(_bounds.x + _bounds.width/2, _bounds.y + _bounds.height/2), this.properties.sides, this.properties.radius/2 || _bounds.height/2);
-	// return new paper.Path.RegularPolygon(new paper.Point(0, 0), 8, 50);
+	return new paper.Path.Rectangle(_bounds);
 };
 
